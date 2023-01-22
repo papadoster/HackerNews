d34380196cec749e16d0fdfd8a8039c509e909af
papadoster <papadoster13@gmail.com>
2023-01-22 17:18:24 +0100

    step 1

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationTitle("Hacker News")
         }
-        .padding()
     }
 }
 
@@ -24,3 +23,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+Post(id: "1", title: "Hello"),
+Post(id: "2", title: "Bye"),
+Post(id: "3", title: "Privet")
+]
